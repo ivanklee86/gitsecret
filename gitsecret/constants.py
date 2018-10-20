b89adcf5a75cc1701688d16d93e7cd3b285f71e6
Ivan Lee <ivanklee86@gmail.com>
2018-10-20 10:00:39 -0400

    Added create() method wrapping "git secret init". Added testing framework.

---
+++ gitsecret/constants.py
@@ -0,0 +1,2 @@
+GIT_SECRET_INIT_COMMAND = "git secret init"
+GIT_SECRET_INIT_REGEX = " created.\\ncleaning up...\\n$"
---
+++ tests/e2e/test_e2e.py
@@ -0,0 +1,23 @@
+from git import InvalidGitRepositoryError
+from git.repo import Repo
+import pytest
+import tempfile
+from gitsecret import GitSecret
+
+
+@pytest.fixture()
+def gen_gitsecret():
+    with tempfile.TemporaryDirectory() as tempdir:
+        Repo.init(tempdir)
+        yield GitSecret(tempdir)
+
+
+def test_gitsecret_invalid_repo(gen_gitsecret):
+    with pytest.raises(InvalidGitRepositoryError):
+        with tempfile.TemporaryDirectory() as tempdir:
+            GitSecret(tempdir)
+
+
+def test_gitsecret(gen_gitsecret):
+    gitsecret = gen_gitsecret
+    gitsecret.create()
